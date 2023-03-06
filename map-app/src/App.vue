92999e95fe857e0461c04ab23f90653833852715
dmurrayROC <120421890+dmurrayROC@users.noreply.github.com>
2023-03-06 12:19:33 -0700

    add reactive text box

--- map-app/src/App.vue
+++ map-app/src/App.vue
@@ -1,13 +1,21 @@
 <template>
+    <button @click="title = 'Changed Popup Title'">Change Title</button>
   <div id="map" />
+  
 </template>
 
 <script>
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { onMounted } from "vue";
+import { createApp, defineComponent, nextTick } from 'vue';
+import MyPopupComponent from '@/components/MyPopupComponent.vue';
+import { ref } from "vue"
 export default {
   setup() {
+    const title = ref("Unchanged Popup Title")
+
+
     onMounted(() => {
       mapboxgl.accessToken =
         "pk.eyJ1IjoiZG11cnJheS1tdCIsImEiOiJjbGV2dTd6N2cwOHBnM3BudGluOXZmMmx2In0.BOEVpTFP8EZUsW8nz1e1lw";
@@ -33,12 +41,21 @@ export default {
         map.on("click", "usa-fill", function (e) {
           new mapboxgl.Popup()
             .setLngLat(e.lngLat)
-            .setHTML('Hello World.')
+            .setHTML('<div id="popup-content"></div>')
             .addTo(map);
+          const MyNewPopup = defineComponent({
+            extends: MyPopupComponent,
+            setup() {
+              return { title }
+            },
+          });
+          nextTick(() => {
+            createApp(MyNewPopup).mount('#popup-content')
+          })
         });
       });
     });
-    return {};
+    return {title};
   },
 };
 </script>
